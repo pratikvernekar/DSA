a37c7fe65bb71b2c816220fd90318bfcb9b05db1
Pratik Vernekar <86291257+pratikvernekar@users.noreply.github.com>
2026-06-27 09:42:52 +0530

    Time: 33 ms (29.29%) | Memory: 54.7 MB (60.35%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,13 +4,9 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    // 1. Iterate over every possible number pair
     for (let i = 0; i < nums.length; i++) {
-        // j is always ahead of i so that we don't re-evaluate already evaluated sums
         for (let j = i + 1; j < nums.length; j++) {
-            // 2. Check if a given pair adds up to our target
             if (nums[i] + nums[j] == target) {
-                // Return the indices when a pair has been found
                 return [i, j];
             }
         }
